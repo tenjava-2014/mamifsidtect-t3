193c9e0c63f52eed45eb60188e17add36aea9db1
Michael Torres <mamifsidtect@gmail.com>
2014-07-12 11:17:47 -0600

    3 more tries, added diamond chances

--- src/main/java/com/tenjava/entries/mamifsidtect/t3/listeners/RandomRareBlockDrops.java
+++ src/main/java/com/tenjava/entries/mamifsidtect/t3/listeners/RandomRareBlockDrops.java
@@ -1,11 +1,14 @@
 package com.tenjava.entries.mamifsidtect.t3.listeners;
 
+import java.util.Random;
+
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class RandomRareBlockDrops implements Listener {
 
@@ -15,7 +18,12 @@ public class RandomRareBlockDrops implements Listener {
 		Block b = event.getBlock();
 		if (b.getType() == Material.DIAMOND_ORE) {
 			if (p.getItemInHand().getType() == Material.DIAMOND_PICKAXE) {
-				//do something here, please work at commit
+				Random r = new Random();
+				Integer chance = r.nextInt(100) - 3;
+				if (chance >= 50) {
+					b.getDrops().clear();
+					b.getDrops().add(new ItemStack(Material.DIAMOND));
+				}
 			}
 		}
 	}
